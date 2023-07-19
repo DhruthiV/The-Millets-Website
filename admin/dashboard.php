2e7e6ae8cf81021869712eefcd02d6a318a2e246
chithrashreen <nchithrashree23@gmail.com>
2023-07-19 17:50:49 +0530

    changed the admin

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -25,18 +25,18 @@ if(!isset($admin_id)){
    <link rel="stylesheet" href="../css/admin_style.css">
 
 </head>
-<body>
+<body style="background-color:#A7EDE7">
 
 <?php include '../components/admin_header.php'; ?>
 
 <section class="dashboard">
 
-   <h1 class="heading">dashboard</h1>
+   <h1  class="heading">dashboard</h1>
 
    <div class="box-container">
 
       <div class="box">
-         <h3>welcome!</h3>
+         <h3>Admin Profile</h3>
          <p><?= $fetch_profile['name']; ?></p>
          <a href="update_profile.php" class="btn">update profile</a>
       </div>
@@ -52,9 +52,9 @@ if(!isset($admin_id)){
                }
             }
          ?>
-         <h3><span>Rs.</span><?= $total_pendings; ?><span>/-</span></h3>
-         <p>total pendings</p>
-         <a href="placed_orders.php" class="btn">see orders</a>
+         <h3>Total Pendings</h3>
+         <p><span>Rs.</span><?= $total_pendings; ?><span>/-</span></p>
+             <a href="placed_orders.php" class="btn">View</a>
       </div>
 
       <div class="box">
@@ -68,9 +68,9 @@ if(!isset($admin_id)){
                }
             }
          ?>
-         <h3><span>Rs.</span><?= $total_completes; ?><span>/-</span></h3>
-         <p>completed orders</p>
-         <a href="placed_orders.php" class="btn">see orders</a>
+         <h3>Completed Orders</h3>
+         <p><span>Rs.</span><?= $total_completes; ?><span>/-</span></p>
+         <a href="placed_orders.php" class="btn">View</a>
       </div>
 
       <div class="box">
@@ -79,9 +79,9 @@ if(!isset($admin_id)){
             $select_orders->execute();
             $number_of_orders = $select_orders->rowCount()
          ?>
-         <h3><?= $number_of_orders; ?></h3>
-         <p>orders placed</p>
-         <a href="placed_orders.php" class="btn">see orders</a>
+         <h3>Placed Orders</h3>
+         <p><?= $number_of_orders; ?></p>
+         <a href="placed_orders.php" class="btn">View</a>
       </div>
 
       <div class="box">
@@ -90,9 +90,9 @@ if(!isset($admin_id)){
             $select_products->execute();
             $number_of_products = $select_products->rowCount()
          ?>
-         <h3><?= $number_of_products; ?></h3>
-         <p>products added</p>
-         <a href="products.php" class="btn">see products</a>
+         <h3> Total products added</h3>
+         <p><?= $number_of_products; ?></p>
+         <a href="products.php" class="btn">View</a>
       </div>
 
       <div class="box">
@@ -101,9 +101,9 @@ if(!isset($admin_id)){
             $select_users->execute();
             $number_of_users = $select_users->rowCount()
          ?>
-         <h3><?= $number_of_users; ?></h3>
-         <p>normal users</p>
-         <a href="users_accounts.php" class="btn">see users</a>
+          <h3>Users</h3>
+         <p><?= $number_of_users; ?></p>
+         <a href="users_accounts.php" class="btn">View</a>
       </div>
 
       <div class="box">
@@ -112,9 +112,9 @@ if(!isset($admin_id)){
             $select_admins->execute();
             $number_of_admins = $select_admins->rowCount()
          ?>
-         <h3><?= $number_of_admins; ?></h3>
-         <p>admin users</p>
-         <a href="admin_accounts.php" class="btn">see admins</a>
+         <h3>Admins</h3>
+         <p><?= $number_of_admins; ?></p>
+         <a href="admin_accounts.php" class="btn">View</a>
       </div>
 
       <div class="box">
@@ -123,9 +123,9 @@ if(!isset($admin_id)){
             $select_messages->execute();
             $number_of_messages = $select_messages->rowCount()
          ?>
-         <h3><?= $number_of_messages; ?></h3>
-         <p>new messages</p>
-         <a href="messagess.php" class="btn">see messages</a>
+         <h3>User messages</h3>
+         <p><?= $number_of_messages; ?></p>
+         <a href="messagess.php" class="btn">View</a>
       </div>
 
    </div>
