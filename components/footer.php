f3c92b836170266d51e8df5d518a4459ef95c2e5
chithrashreen <nchithrashree23@gmail.com>
2023-07-19 17:51:03 +0530

    footer

---
+++ components/footer.php
@@ -0,0 +1,59 @@
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+   
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+    
+
+</head>
+<footer class="footer">
+    <div class="cont">
+    
+      <div class="link-boxes">
+        <ul class="f-box">
+          <li class="link_name">Information</li>
+          <li><a href="../home.php">Home</a></li>
+          <li><a href="../contact.php">Contact us</a></li>
+          <li><a href="../about.php">About us</a></li>
+          <li><a href="../home.php">shop</a></li>
+        </ul>
+        <ul class="f-box">
+          <li class="link_name">Services</li>
+          <li><a href="../cart.php">Cart</a></li>
+          <li><a href="../orders.php">Orders</a></li>
+          <li><a href="#">Gifts & Offer</a></li>
+          <li><a href="#">Payment</a></li>
+
+        </ul>
+        <ul class="f-box">
+          <li class="link_name">Account</li>
+          <li><a href="#">Profile</a></li>
+          <li><a href="#">Log in </a></li>
+          <li><a href="#">Log out</a></li></ul>
+        <ul class="f-box">
+            <li class="link_name">Follow us</li>
+            <li> <a href="#"><i class="fab fa-facebook-f"></i>  facebook</a></li>
+           <li><a href="#"><i class="fab fa-twitter"></i>   twitter</a></li>
+            <li><a href="#"><i class="fab fa-instagram"></i>   instagram</a></li>
+           <li> <a href="#"><i class="fab fa-linkedin"></i>  linkedin</a></li>
+        </ul>
+        <ul class="f-box input-box">
+          <li class="link_name">Contact us</li>
+          <li class="mob">080-655-244</li>
+        </ul>
+      </div>
+    </div>
+    <div class="bottom-details">
+      <div class="bottom_text">
+        <span class="copyright_text">Copyright © 2023 <a href="#">Siri Flavors</a>All rights reserved</span>
+        <span class="policy_terms">
+          <a href="#">Privacy policy</a>
+          <a href="#">Terms & condition</a>
+        </span>
+      </div>
+    </div>
+  </footer>
